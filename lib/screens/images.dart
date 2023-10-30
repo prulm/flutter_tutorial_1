1ea26acaf19ac61e534e44e10469b13ed78d7188
Prulm <jaredyared83@gmail.com>
2023-10-30 19:31:55 +0300

    more features with images

--- lib/screens/images.dart
+++ lib/screens/images.dart
@@ -6,9 +6,35 @@ class Images extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          // child: Image.network('https://images.unsplash.com/photo-1698551899401-bd5391406d6f?auto=format&fit=crop&q=80&w=464&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
-          child: Image.asset('assets/images/fashion.jpg'),
+      body: ListView(
+        children: [
+          Image.network('https://images.unsplash.com/photo-1698551899401-bd5391406d6f?auto=format&fit=crop&q=80&w=464&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
+          ElevatedButton(
+            onPressed: () {
+              print('what is up?');
+            },
+            child: Icon(Icons.whatshot_outlined, color: Colors.white),
+            style: ElevatedButton.styleFrom(
+              primary: Colors.lightBlueAccent,
+              onPrimary: Colors.pink,
+              elevation: 5,
+              fixedSize: Size(50, 50),
+            ),
+          ),
+          Image.asset('assets/images/fashion.jpg'),
+          ElevatedButton(
+            onPressed: () {
+              print('what is up?');
+            },
+            child: Icon(Icons.whatshot_outlined, color: Colors.pink),
+            style: ElevatedButton.styleFrom(
+              primary: Colors.lightBlueAccent,
+              onPrimary: Colors.white,
+              elevation: 5,
+              fixedSize: Size(50, 50),
+            ),
+          ),
+        ],
       ),
     );
   }
